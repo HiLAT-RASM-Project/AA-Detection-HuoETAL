377d8acf062610bbadee2162bcf4bc5082c48588
YilingHuo <127141605+YilingHuo@users.noreply.github.com>
2024-10-11 10:51:36 -0700

    Update and rename planck_lapserate_albedo.py to planck_lapserate_albedo_watervapor.py

--- planck_lapserate_albedo_watervapor.py
+++ planck_lapserate_albedo_watervapor.py
@@ -148,7 +148,7 @@ if verticalflip:    hus = np.flip(hus,axis=1)
 q1=np.zeros([12,nlev,nlat,nlon])
 for imonth in range(12):
     q1[imonth]=np.nanmean(hus[imonth::12],axis=0)
-rh = q1/qs1;del q1
+rh = q1/qs1
 # the change in moisture
 dq=dataanomaly(hus);del hus
 # Read kernels
@@ -190,10 +190,12 @@ dLW_q=tas*0;dSW_q=tas*0
 for iyear in range(nyear):
     month1=iyear*12;month2=iyear*12+12
     dqdt = (qs2[month1:month2] - qs1)/ta[month1:month2]*rh
+    ###dlogqdt;dlogq;Uses the fractional change approximation of logarithms in the specific humidity response & normalization factor
+    dqdt=dqdt/q1; dq[month1:month2]=dq[month1:month2]/q1 
     # Normalize kernels by the change in moisture for 1 K warming at constant RH; Convolve moisture kernel with change in moisture
     dLW_q[month1:month2]=np.nansum(q_LW_kernel/dqdt*dq[month1:month2]*pdiff,axis=1)
     dSW_q[month1:month2]=np.nansum(q_SW_kernel/dqdt*dq[month1:month2]*pdiff,axis=1)   
-del qs1,qs2,dqdt
+del qs1,qs2,dqdt,q1
 
 
 
